1b014ec3847af477ef2d0f7db6a4125b1c38eb70
RobyTako <roby.arjuna@tako.co.id>
2026-01-22 23:36:03 +0700

    setting pagination

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrap();
+        Paginator::currentPathResolver(function () {
+            return url()->current();
+        });
         if (app()->environment('local')) {
             URL::forceRootUrl(config('app.url'));
         }
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\MCompany;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -16,17 +17,39 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'username' => 'test',
-            'password' => Hash::make('password'),
-            'user_token' => Hash::make('test@example.com')
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        //     'username' => 'test',
+        //     'password' => Hash::make('password'),
+        //     'user_token' => Hash::make('test@example.com')
+        // ]);
 
-        $this->call([
-            RolePermissionSeeder::class,
-            UserRoleSeeder::class
-        ]);
+        // $this->call([
+        //     RolePermissionSeeder::class,
+        //     UserRoleSeeder::class
+        // ]);
+
+        $totalRecords = 100000; // total data
+        $batchSize = 5000;         // insert per batch
+        $levels = ['HOLDING', 'COMPANY'];
+
+        for ($i = 0; $i < $totalRecords; $i += $batchSize) {
+            $companies = [];
+
+            for ($j = 0; $j < $batchSize; $j++) {
+                $companies[] = [
+                    'company_name' => 'PT Rejosoft Cemerlang ' . ($i + $j + 1),
+                    'level' => $levels[array_rand($levels)],
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+
+            MCompany::insert($companies);
+
+            // echo "Inserted " . min($i + $batchSize, $totalRecords) . " / $totalRecords companies\n";
+        }
     }
 }
+
