88403e339a39c74d80f680a1d4c85a70534d08f9
laravelisme <agung.nugroho@tako.co.id>
2026-02-05 14:50:16 +0700

    Update footer to include dynamic app name and current year

--- src/resources/views/components/footer.blade.php
+++ src/resources/views/components/footer.blade.php
@@ -1,7 +1,8 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p><script>document.write(new Date().getFullYear())</script> &copy; PT. TAKO ANUGERAH KORPORASI.</p>
+            <p> &copy; <script>document.write(new Date().getFullYear())</script>
+                {{ $global_setting['app_name'] ?? 'Company' }}</p>
         </div>
     </div>
 </footer>
